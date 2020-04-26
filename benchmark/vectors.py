485d95a6742be7af53fe784d849fd8931bf906cc
Christian Puhrsch <cpuhrsch@fb.com>
2020-04-26 13:48:55 -0400

    Checkpoint

--- benchmark/vectors.py
+++ benchmark/vectors.py
@@ -8,8 +8,7 @@ def benchmark_fasttext():
     ft = vocab.FastText()
     train, test = experimental.datasets.raw.AG_NEWS()
     tokenizer = get_tokenizer('basic_english')
-    # strings = list(tokenizer(s) for (l, s) in train)
-    strings = list(_torchtext.StringList(tokenizer(s)) for (l, s) in train)
+    strings = list(tokenizer(s) for (l, s) in train)
     print("starting")
     t0 = time.monotonic()
     for sl in strings:
--- torchtext/csrc/vocab.cpp
+++ torchtext/csrc/vocab.cpp
@@ -16,47 +16,6 @@ uint32_t hash(const std::string &str) {
   return hash(str.c_str(), strlen(str.c_str()));
 }
 
-struct StringList {
-  StringList(const std::vector<std::string>& strs) 
-  {
-    num_strings = strs.size();
-    strlens = static_cast<size_t *>(malloc(sizeof(size_t) * num_strings));
-    size_t length = 0;
-    for (size_t i = 0; i < num_strings; i++) {
-      const char *c_str = strs[i].c_str();
-      size_t s_len = strlen(c_str);
-      strlens[i] = s_len;
-      length += s_len;
-    }
-    size_t index = 0;
-    data = static_cast<char *>(malloc(sizeof(char) * length));
-    for (size_t i = 0; i < num_strings; i++) {
-      const char *c_str = strs[i].c_str();
-      memcpy(data + index, c_str, strlens[i]);
-      index += strlens[i];
-    }
-  }
-
-  std::string __str__() {
-    std::stringstream ss;
-    size_t length = 0;
-    for (size_t i = 0; i < num_strings; i++) {
-      length += strlens[i];
-    }
-    for (size_t i = 0; i < length; i++) {
-      ss << data[i];
-    }
-    ss << " - ";
-    ss << std::vector<int64_t>(strlens, strlens + num_strings);
-    return ss.str();
-  }
-
-  char* data;
-  size_t* strlens;
-  size_t num_strings;
-
-};
-
 struct Vocab {
   Vocab(std::vector<std::string> itos, at::Tensor vectors,
         at::Tensor unk_vector)
@@ -78,28 +37,10 @@ struct Vocab {
     return _vectors[search->second];
   }
   int64_t __len__() { return _unk_index; }
-  at::Tensor get_vecs_by_tokens(const StringList &tokens) {
-    int64_t index = 0;
-    at::Tensor indices = torch::empty({int64_t(tokens.num_strings)}, at::TensorOptions(torch::Dtype::Long));
-    auto indices_accessor = indices.accessor<int64_t, 1>();
-    char* data = tokens.data;
-    size_t* strlens = tokens.strlens;
-    size_t offset = 0;
-    for (size_t i = 0; i < tokens.num_strings; i++) {
-      auto search = _map.find(hash(data + offset, strlens[i]));
-      offset += strlens[i];
-      if (search != _map.end()) {
-        indices_accessor[index] = search->second;
-      } else {
-        indices_accessor[index] = _unk_index;
-      }
-      index++;
-    }
-    return at::index_select(_vectors, 0, indices);
-  }
   at::Tensor get_vecs_by_tokens(const std::vector<std::string> &tokens) {
     int64_t index = 0;
-    at::Tensor indices = torch::empty({int64_t(tokens.size())}, at::TensorOptions(torch::Dtype::Long));
+    at::Tensor indices = torch::empty({int64_t(tokens.size())},
+                                      at::TensorOptions(torch::Dtype::Long));
     auto indices_accessor = indices.accessor<int64_t, 1>();
     for (const std::string &token : tokens) {
       auto search = _map.find(hash(token));
@@ -120,10 +61,6 @@ private:
 };
 
 PYBIND11_MODULE(_torchtext, m) { 
-    auto c1 = py::class_<StringList>(m, "StringList");
-    c1.def(py::init<const std::vector<std::string> &>());
-    c1.def("__str__", &StringList::__str__);
-
     auto c = py::class_<Vocab>(m, "Vocab");
     c.def(py::init<
             const std::vector<std::string>&, // stoi
@@ -132,9 +69,5 @@ PYBIND11_MODULE(_torchtext, m) {
          );
     c.def("__getitem__", &Vocab::__getitem__);
     c.def("__len__", &Vocab::__len__);
-    c.def("get_vecs_by_tokens",
-          py::overload_cast<const std::vector<std::string> &>(
-              &Vocab::get_vecs_by_tokens));
-    c.def("get_vecs_by_tokens",
-          py::overload_cast<const StringList &>(&Vocab::get_vecs_by_tokens));
+    c.def("get_vecs_by_tokens", &Vocab::get_vecs_by_tokens);
 }
