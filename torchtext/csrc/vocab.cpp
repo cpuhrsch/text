b4fc30eaab36e067f6884619ed38afb11acada44
Christian Puhrsch <cpuhrsch@fb.com>
2020-04-26 01:21:11 -0400

    Checkpoint

--- torchtext/csrc/vocab.cpp
+++ torchtext/csrc/vocab.cpp
@@ -4,42 +4,44 @@
 struct Vocab {
   Vocab(std::vector<std::string> itos, at::Tensor vectors,
         at::Tensor unk_vector)
-      : _unk_vector(unk_vector) {
-
+      : _vectors(
+            at::cat({vectors, unk_vector.reshape({1, unk_vector.size(0)})})) {
     int64_t index = 0;
     _map.reserve(itos.size());
     for (const std::string & t : itos) {
       _map.insert({t, index});
       index++;
     }
-
-    index = 0;
-    _vector_list.resize(vectors.size(0));
-    for (at::Tensor t : vectors.unbind(0)) {
-      _vector_list[index] = t;
-      index++;
-    }
   }
   at::Tensor __getitem__(const std::string &token) {
     auto search = _map.find(token);
     if (search == _map.end()) {
-      return _unk_vector;
+      return _vectors[_vectors.size(0) - 1].clone();
     }
-    return _vector_list[search->second];
+    return _vectors[search->second].clone();
   }
-  int64_t __len__() { return _vector_list.size(); }
+  // -1 because of unk_vector
+  int64_t __len__() { return _vectors.size(0) - 1; }
   at::Tensor get_vecs_by_tokens(const std::vector<std::string> &tokens) {
-    std::vector<at::Tensor> indices;
+    std::vector<int64_t> indices;
+    indices.resize(tokens.size());
+    int64_t index = 0;
     for (const std::string &token : tokens) {
-      indices.push_back(__getitem__(token));
+      auto search = _map.find(token);
+      if (search == _map.end()) {
+        indices[index] = _vectors.size(0) - 1;
+      } else {
+        indices[index] = search->second;
+      }
+      index++;
     }
-    return at::stack(indices);
+    at::Tensor ind = torch::tensor(indices);
+    return at::index_select(_vectors, 0, ind);
   }
 
 private:
   std::unordered_map<std::string, int64_t> _map;
-  std::vector<at::Tensor> _vector_list;
-  at::Tensor _unk_vector;
+  at::Tensor _vectors;
   int64_t _dim;
 };
 
